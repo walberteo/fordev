8f973fe07b54b61a79d1c49009172684a10d8292
Walber Teodoro <walberteo@gmail.com>
2020-09-21 10:45:51 -0300

    move SUT creation to the setUp method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -25,12 +25,20 @@ class HttpAdapter {
 }
 
 void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+
   group('post', () {
     test('Should call post whit correct values', () async {
       // arrange
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
+
       // act
       await sut.request(url: url, method: 'post');
       // assert
