a67c9d23e06fefe312a8e70024ec05712f1f9fb3
Walber Teodoro <walberteo@gmail.com>
2020-09-21 16:12:57 -0300

    ensure HttpAdapter throws Anauthorized on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -126,6 +126,15 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test('Should return UnauthorizedError if post returns 401', () async {
+      // arrange
+      mockResponse(401);
+      // act
+      final future = sut.request(url: url, method: 'post');
+
+      // assert
+      expect(future, throwsA(HttpError.unauthorized));
+    });
     test('Should return ServerError if post returns 500', () async {
       // arrange
       mockResponse(500);
